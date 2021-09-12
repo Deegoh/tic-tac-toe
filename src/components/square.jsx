6de68f048c2da8137904596d907cb94ef47d99a6
Deegoh <tiagopintomonteiro@gmail.com>
2021-09-12 23:37:58 +0200

    trying to use state with airbnb style guide

--- src/components/square.jsx
+++ src/components/square.jsx
@@ -1,13 +1,22 @@
+/* eslint-disable no-undef */
 /* eslint-disable jsx-a11y/control-has-associated-label */
 /* eslint-disable react/button-has-type */
 /* eslint no-console: ["error", { allow: ["log"] }] */
-import React from 'react';
+import React, { useState } from 'react';
 
-const Square = () => (
-  <button
-    className="square"
-    onClick={() => { console.log('x'); }}
-  />
-);
+// eslint-disable-next-line react/prop-types
+function Square(value) {
+  const [valueGrid, setValueGrid] = useState(value);
+
+  return (
+    <button
+      className="square"
+      onClick={() => {
+        setValueGrid({ valueGrid });
+        console.log({ valueGrid });
+      }}
+    />
+  );
+}
 
 export default Square;
